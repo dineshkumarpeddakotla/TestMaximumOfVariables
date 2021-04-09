efc633059d96c3fbdedaad985dafaa7e453849ba
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-09 21:19:55 +0530

    Created: MaximumOfVariables and TestMaximumOfVariables files

---
+++ src/main/java/com/maximumofvariables/MaximumOfVariables.java
@@ -0,0 +1,8 @@
+package com.maximumofvariables;
+
+public class MaximumOfVariables {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Maximum of variables");
+    }
+}
