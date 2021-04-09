582d129a86be62c37681c0748ab4ccac55581241
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-09 22:01:33 +0530

    ADDED: TestCase1.2, Maximum Integer at Second Position

--- src/test/java/TestMaximumOfVariables.java
+++ src/test/java/TestMaximumOfVariables.java
@@ -1,4 +1,13 @@
+import com.maximumofvariables.MaximumOfVariables;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class TestMaximumOfVariables {
+    MaximumOfVariables testObj = new MaximumOfVariables();
 
+    @Test
+    void GivenIntegerArray_MaxInteger_SecondPosition_ReturnsTrue() {
+        int testMaximum = testObj.findMaxInteger(25, 89, 65);
+        Assertions.assertEquals(89, testMaximum);
+    }
 }
