152c038fac35a12cca7b0950b6127406d99452ab
dinnu247 <dineshkumar.icon@gmail.com>
2021-04-10 07:57:40 +0530

    MODIFIED:to Take More Than 3 Varaibles and Find Maximum

--- src/main/java/com/maximumofvariables/MaximumOfVariables.java
+++ src/main/java/com/maximumofvariables/MaximumOfVariables.java
@@ -1,24 +1,36 @@
 package com.maximumofvariables;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class MaximumOfVariables <E extends Comparable<E>> {
 
-    E firstVariable, secondVariable, thirdVariable;
+    E firstVariable, secondVariable, thirdVariable, fourthVariable, fifthVariable;
 
-    public MaximumOfVariables(E firstVariable, E secondVariable, E thirdVarible){
+    public MaximumOfVariables(E firstVariable, E secondVariable, E thirdVariable, E fourthVariable, E fifthVariable){
         this.firstVariable = firstVariable;
         this.secondVariable = secondVariable;
-        this.thirdVariable = thirdVarible;
-
+        this.thirdVariable = thirdVariable;
+        this.fourthVariable = fourthVariable;
+        this.fifthVariable = fifthVariable;
     }
 
-    public E findMaxVariable(E firstVariable,E secondVariable,E thirdVariable) {
+    public static <E extends Comparable<E>>  E multipleVariable(E firstVariable,E secondVariable,E thirdVariable,E fourthVariable, E fifthVariable) {
 
-        if (firstVariable.compareTo(secondVariable) > 0 && firstVariable.compareTo(thirdVariable) > 0)
-            return firstVariable;
+        List<E> variableList = new ArrayList<>();
+        variableList.add(firstVariable);
+        variableList.add(secondVariable);
+        variableList.add(thirdVariable);
+        variableList.add(fourthVariable);
+        variableList.add(fifthVariable);
 
-        if (secondVariable.compareTo(thirdVariable) > 0)
-            return secondVariable;
-
-        return thirdVariable;
+        variableList.sort(Comparator.naturalOrder());
+        return variableList.get(4);
     }
+
+    public  E findMaxVariable(){
+        return MaximumOfVariables.multipleVariable(firstVariable,secondVariable,thirdVariable,fourthVariable,fifthVariable);
+    }
+
 }
--- src/test/java/TestMaximumOfVariables.java
+++ src/test/java/TestMaximumOfVariables.java
@@ -5,64 +5,23 @@ import org.junit.jupiter.api.Test;
 public class TestMaximumOfVariables {
 
     @Test
-    void GivenIntegerArray_MaxInteger_FirstPosition_ReturnsTrue() {
-        MaximumOfVariables<Integer> testObj = new MaximumOfVariables<>(89,25 ,65 );
-        Comparable<Integer> testMaximum = testObj.findMaxVariable(89, 25, 65);
+    void GivenIntegerArray_MaxInteger_ReturnsTrue() {
+        MaximumOfVariables<Integer> testObj = new MaximumOfVariables<>(89, 25, 65,55,7);
+        Comparable<Integer> testMaximum = testObj.findMaxVariable();
         Assertions.assertEquals(89, testMaximum);
     }
 
     @Test
-    void GivenIntegerArray_MaxInteger_SecondPosition_ReturnsTrue() {
-        MaximumOfVariables<Integer> testObj = new MaximumOfVariables<>(25,89 ,65 );
-        Comparable<Integer> testMaximum = testObj.findMaxVariable(25, 89, 65);
-        Assertions.assertEquals(89, testMaximum);
-    }
-
-    @Test
-    void GivenIntegerArray_MaxInteger_ThirdPosition_ReturnsTrue() {
-        MaximumOfVariables<Integer> testObj = new MaximumOfVariables<>(25,65 ,89 );
-        Comparable<Integer> testMaximum = testObj.findMaxVariable(25, 65, 89);
-        Assertions.assertEquals(89, testMaximum);
-    }
-
-    @Test
-    void GivenFloatArray_MaxFloat_FirstPosition_ReturnsTrue() {
-        MaximumOfVariables<Float> testObj = new MaximumOfVariables<>(89.25f,25.85f ,65.69f );
-        Comparable<Float> testMaximum = testObj.findMaxVariable(89.25f, 25.85f, 65.69f);
+    void GivenFloatArray_MaxFloat_ReturnsTrue() {
+        MaximumOfVariables<Float> testObj = new MaximumOfVariables<>(89.25f,25.85f ,65.69f,25.68f,64.89f );
+        Comparable<Float> testMaximum = testObj.findMaxVariable();
         Assertions.assertEquals(89.25f, testMaximum);
     }
 
     @Test
-    void GivenFloatArray_MaxFloat_SecondPosition_ReturnsTrue() {
-        MaximumOfVariables<Float> testObj = new MaximumOfVariables<>( 25.58f, 89.59f, 65.62f);
-        Comparable<Float> testMaximum = testObj.findMaxVariable(25.58f, 89.59f, 65.62f);
-        Assertions.assertEquals(89.59f, testMaximum);
-    }
-
-    @Test
-    void GivenFloatArray_MaxFloat_ThirdPosition_ReturnsTrue() {
-        MaximumOfVariables<Float> testObj = new MaximumOfVariables<>(25.56f, 65.95f, 89.85f);
-        Comparable<Float> testMaximum = testObj.findMaxVariable(25.56f, 65.95f, 89.85f);
-        Assertions.assertEquals(89.85f, testMaximum);
-    }
-    @Test
-    void GivenStringArray_MaxString_FirstPosition_ReturnsTrue() {
-        MaximumOfVariables<String> testObj = new MaximumOfVariables<>("Peach","Apple" ,"Banana" );
-        Comparable<String> testMaximum = testObj.findMaxVariable("Peach", "Apple", "Banana");
-        Assertions.assertEquals("Peach", testMaximum);
-    }
-
-    @Test
-    void GivenStringArray_MaxString_SecondPosition_ReturnsTrue() {
-        MaximumOfVariables<String> testObj = new MaximumOfVariables<>("Apple","Peach" ,"Banana" );
-        Comparable<String> testMaximum = testObj.findMaxVariable("Apple", "Peach", "Banana");
-        Assertions.assertEquals("Peach", testMaximum);
-    }
-
-    @Test
-    void GivenStringArray_MaxString_ThirdPosition_ReturnsTrue() {
-        MaximumOfVariables<String> testObj = new MaximumOfVariables<>("Banana","Apple" ,"Peach");
-        Comparable<String> testMaximum = testObj.findMaxVariable("Banana", "Apple", "Peach");
+    void GivenStringArray_MaxString_ReturnsTrue() {
+        MaximumOfVariables<String> testObj = new MaximumOfVariables<>("Apple","Peach" ,"Banana", "Grape","Mango");
+        Comparable<String> testMaximum = testObj.findMaxVariable();
         Assertions.assertEquals("Peach", testMaximum);
     }
 }
